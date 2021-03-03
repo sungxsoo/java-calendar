51e9c20bed5999f94c25dcb642ff4957df82f54b
sungxsoo <njs05053@naver.com>
2021-03-03 18:21:20 +0900

    drawing Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31,30,31};
+	
 	public int getmaxDaysofMonth(int month) {
 		return MAX_DAYS[month-1];
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public void printCalendar(int year, int month) {
+		System.out.printf("		<<%4d년 %3d월>>\n", year, month);
+		System.out.println("일	월	화	수	목	금	토");
+		System.out.println("----------------------------------------------------");
 		
-
-		Scanner sc = new Scanner(System.in);
+		int maxDay = getmaxDaysofMonth(month);
+		for(int i=1; i<maxDay; i++) {
+			if(i%7==0) {
+				System.out.printf("\n");
+			}
+			System.out.print(i + "	");
+		}
 		
-		Calendar cal = new Calendar();
+		System.out.println();
 		
-		System.out.println("달을 입력하세요.");
-		
-		int month = sc.nextInt();
-		
-		System.out.printf("%d월은 %d일 까지 있습니다\n", month, cal.getmaxDaysofMonth(month));
-
-		sc.close();
+//		System.out.println("1	2	3	4	5	6	7");
+//		System.out.println("8	9	10	11	12	13	14");
+//		System.out.println("15	16	17	18	19	20	21");
+//		System.out.println("22	23	24	25	26	27	28");
+//		System.out.println("29	30	31");
 	}
-
 }
---
+++ src/calendar/Prompt.java
@@ -0,0 +1,36 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private static final String PROMPT = "cal>";
+	
+	public void runPrompt() {
+
+		Scanner sc = new Scanner(System.in);
+		Calendar cal = new Calendar();  
+		int month = 0;
+		int year;
+		
+		
+		while(true) {
+			System.out.println("달을 입력하세요.");
+			
+			System.out.print(PROMPT);
+			
+			month = sc.nextInt();
+	
+			if(month==-1||month>12) {
+				break;
+			}	
+			
+			cal.printCalendar(2017, month);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Prompt prompt = new Prompt();
+		prompt.runPrompt();
+	}
+}
