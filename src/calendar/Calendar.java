23bfe33e8b71da6d0ecc47ff12775f4414778a9b
sungxsoo <njs05053@naver.com>
2021-03-03 16:41:44 +0900

    Fix encoding

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,7 +5,9 @@ public class Calendar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		System.out.println("Hello, Calendar");
+		for(int i = 0; i<7; i++) {
+			
+		}
 	}
 
 }
